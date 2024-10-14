295e75645a6039d7285eca86aa82420d7891eade
MarkLysenkoZp <marcuslysenko061@gmail.com>
2024-10-14 13:58:19 +0300

    exercise 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      flash.now[:danger] = "Invalid signup information"
       render 'new'
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,16 +2,30 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "password is too simple" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "Example User",
-                                          email: "user@example.com",
-                                          password: "password",
-                                          password_confirmation: "password" } }
+      post users_path, params: { user: { name:  "",
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.alert', /The form contains 1 error/
+    
+ 
+    assert_not flash.empty?
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                          email: "user@example.com",
+                                          password: "password123",
+                                          password_confirmation: "password123" } }
+    end
+    follow_redirect! 
+    assert_template 'users/show'  
+    assert_not flash.empty?  
   end
 end
